7d93f7699100952dbac60c0c58c24ddf1fb9ecd8
AnastasyaOsipova <nastasjaosipova@yandex.ru>
2021-12-23 11:43:52 +0300

    поправила по первому ревью

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,77 +1,88 @@
-import React from 'react';
-import api from '../utils.js/Api';
-import Card from './Card';
+import React from "react";
+import api from "../utils.js/Api";
+import Card from "./Card";
 
 function Main(props) {
+  const [userName, setUserName] = React.useState(null);
+  const [userDescription, setUserDescription] = React.useState(null);
+  const [userAvatar, setUserAvatar] = React.useState(null);
 
-const [userName, setUserName] = React.useState(null);
-const [userDescription, setUserDescription] = React.useState(null);
-const [userAvatar, setUserAvatar] = React.useState(null);
+  const [cards, setCards] = React.useState([]);
 
-const [cards, setCards] = React.useState([]);
-
-    
-React.useEffect(()=>{
-    api.getUserInfoApi()
-    .then(data => {
+  React.useEffect(() => {
+    api
+      .getUserInfoApi()
+      .then((data) => {
         setUserName(data.name);
         setUserDescription(data.about);
-        setUserAvatar(data.avatar)
-    })
-});
+        setUserAvatar(data.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-React.useEffect(()=>{
-    api.getInitialCards()
-    .then(data =>{
-     const cards = data.map(item =>{
-            return{
-                id: item._id,
-                image: item.link,
-                title: item.name,
-                alt: item.name,
-                likes: item.likes.length
-                
-     }
-    })
-    setCards(cards)
-    })
-})
+  React.useEffect(() => {
+    api
+      .getInitialCards()
+      .then((data) => {
+        const cards = data.map((item) => {
+          return {
+            id: item._id,
+            image: item.link,
+            title: item.name,
+            alt: item.name,
+            likes: item.likes.length,
+          };
+        });
 
-    
-    
-return (
+        setCards(cards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
     <main className="main">
-        <section className="profile">
-            <div className="profile__info">
-                <div onClick={props.onEditAvatar} className="profile__avatar-edit-icon">
-                    <img src={userAvatar} alt="Фото" className="profile__avatar"/>
-                </div>
-                <div className="profile__text">
-                    <h1 className="profile__name">{userName}</h1>
-                    <p className="profile__description">{userDescription}</p>
-                </div>
-                <button onClick={props.onEditProfile} type="button" className="profile__edit-button button"></button>              
-            </div>
-            <button onClick={props.onAddPlace} type="button" className="profile__add-button button"></button>
-        </section>
-        <section className="photo-grid">
-        {
-              cards.map(item =>
-                <Card
-                    key={item.id}
-                    src={item.image}
-                    title={item.title}
-                    alt={item.alt}
-                    likes={item.likes}
-                    onCardClick={props.handleClick}
-                />
-                ) 
-           }
-        </section>
+      <section className="profile">
+        <div className="profile__info">
+          <div
+            onClick={props.onEditAvatar}
+            className="profile__avatar-edit-icon"
+          >
+            <img src={userAvatar} alt="Фото" className="profile__avatar" />
+          </div>
+          <div className="profile__text">
+            <h1 className="profile__name">{userName}</h1>
+            <p className="profile__description">{userDescription}</p>
+          </div>
+          <button
+            onClick={props.onEditProfile}
+            type="button"
+            className="profile__edit-button button"
+          ></button>
+        </div>
+        <button
+          onClick={props.onAddPlace}
+          type="button"
+          className="profile__add-button button"
+        ></button>
+      </section>
+      <section className="photo-grid">
+        {cards.map((item) => (
+          <Card
+            key={item.id}
+            src={item.image}
+            title={item.title}
+            alt={item.alt}
+            likes={item.likes}
+            onCardClick={props.handleClick}
+          />
+        ))}
+      </section>
     </main>
   );
 }
 
-
-
-export default Main
+export default Main;
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,30 +1,34 @@
 import React from "react";
 
-
-function PopupWithForm(props){
-    return(
-        <div>
-        {props.isOpen ?
-            (<div className={`popup popup_type_${props.name} popup_opened`}>
-            <div className="popup__container">
-                <button onClick={props.onClose} type="button" className="popup__close-button button"></button>
-                <form noValidate id="2" className="popup__form" name="`${props.name}_form`">
-                    <h2 className="popup__title">{props.title}</h2>
-                    {props.children}
-                </form>
-            </div>
-            </div>) :
-            (<div className={`popup popup_type_${props.name}`}>
-            <div className="popup__container">
-                <button type="button" className="popup__close-button button"></button>
-                <form noValidate id="2" className="popup__form" name="`${props.name}_form`">
-                    <h2 className="popup__title">{props.title}</h2>
-                    {props.children}
-                </form>
-            </div>
-            </div>)}
+function PopupWithForm(props) {
+  return (
+    <div>
+      {props.isOpen && (
+        <div
+          className={`popup popup popup_type_${props.name} ${
+            props.isOpen ? "popup_opened" : ""
+          }`}
+        >
+          <div className="popup__container">
+            <button
+              onClick={props.onClose}
+              type="button"
+              className="popup__close-button button"
+            ></button>
+            <form
+              noValidate
+              id="2"
+              className="popup__form"
+              name="`${props.name}_form`"
+            >
+              <h2 className="popup__title">{props.title}</h2>
+              {props.children}
+            </form>
+          </div>
+        </div>
+      )}
     </div>
-    )
+  );
 }
 
-export default PopupWithForm
+export default PopupWithForm;
